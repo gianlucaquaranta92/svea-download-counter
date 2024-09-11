7141aad3526428d533afe9c77e6e22596996bc35
gianluca <gianlucaquaranta92@gmail.com>
2024-09-11 13:34:55 +0200

    created the callback function to handle the get request and show the downloads amount numbers

--- svea-download-counter.php
+++ svea-download-counter.php
@@ -1,13 +1,13 @@
 <?php
 /*
 Plugin Name: SVEA Checkout Downloads Counter
-Description: Create a notice in the admin panel to show in real time the downloads of the SVEA WP-plugin.
+Description: Create a widghet in the admin dashboard to show in real time the downloads of the SVEA WP-plugin.
 Version: 1.0
 Author: Gianluca
 */
 
 
-// Hook to set up the dashboard widget
+// Dashboard widget
 add_action('wp_dashboard_setup', 'svea_add_dashboard_widget');
 
 function svea_add_dashboard_widget() {
@@ -18,7 +18,27 @@ function svea_add_dashboard_widget() {
     );
 }
 
-// Function to display content in the widget
+// Display content inside the widget
 function svea_dashboard_widget_display() {
-echo 'hi im a widget in the dashboard';
+
+
+    // Fetch plugin download stats 
+    $plugin_slug = 'svea-checkout-for-woocommerce';  
+    $response = wp_remote_get("https://api.wordpress.org/plugins/info/1.1/?action=plugin_information&request[slug]={$plugin_slug}");
+    
+    // Check the API response 
+    if (is_wp_error($response)) {
+        echo '<p>Something went wrong,could not fetch the data.</p>';
+        return;
+    }
+
+    $plugin_data = json_decode(wp_remote_retrieve_body($response));
+
+    if (isset($plugin_data->downloaded)) {
+   
+        echo '<p>Total Downloads: ' . $plugin_data->downloaded . '</p>';
+        echo '<p>Last updated: ' . $plugin_data->last_updated . '</p>';
+    } else {
+        echo '<p>Something went wrong,could not get the downloaded data.</p>';
+    }
 }
