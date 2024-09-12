47f78acdbe526d670432148e870abc13459f3314
gianluca <gianlucaquaranta92@gmail.com>
2024-09-12 11:00:04 +0200

    -cached the fetched data for 24h

--- svea-download-counter.php
+++ svea-download-counter.php
@@ -18,27 +18,46 @@ function svea_add_dashboard_widget() {
     );
 }
 
-// Display content inside the widget
 function svea_dashboard_widget_display() {
-
-
-    // Fetch plugin download stats 
-    $plugin_slug = 'svea-checkout-for-woocommerce';  
-    $response = wp_remote_get("https://api.wordpress.org/plugins/info/1.1/?action=plugin_information&request[slug]={$plugin_slug}");
-    
-    // Check the API response 
-    if (is_wp_error($response)) {
-        echo '<p>Something went wrong,could not fetch the data.</p>';
-        return;
-    }
-
-    $plugin_data = json_decode(wp_remote_retrieve_body($response));
-
-    if (isset($plugin_data->downloaded)) {
+   // Check if the data is cached
+    $transient_key = 'svea_download_count';
+    $plugin_data = get_transient($transient_key);
    
+    // If it is cached
+    if ($plugin_data !== false) {
+
+        // Display cached data
         echo '<p>Total Downloads: ' . $plugin_data->downloaded . '</p>';
         echo '<p>Last updated: ' . $plugin_data->last_updated . '</p>';
+
+    // If not cached
     } else {
-        echo '<p>Something went wrong,could not get the downloaded data.</p>';
+        // Fetch plugin stats 
+        $plugin_slug = 'svea-checkout-for-woocommerce';  
+        $response = wp_remote_get("https://api.wordpress.org/plugins/info/1.1/?action=plugin_information&request[slug]={$plugin_slug}");
+
+        // Check the API response 
+        if (is_wp_error($response)) {
+            echo '<p>Something went wrong, could not fetch the data.</p>';
+            return;
+        }
+
+        //  Retrieve the data and decode it in a php format
+        $plugin_data = json_decode(wp_remote_retrieve_body($response));
+     
+        // If downloaded has value
+        if (isset($plugin_data->downloaded)) {
+            echo '<p>Total Downloads: ' . $plugin_data->downloaded . '</p>';
+            echo '<p>Last updated: ' . $plugin_data->last_updated . '</p>';
+            
+         // Set a cache for the duration of 24h based on the time of update of the API(once a day)
+            set_transient($transient_key, $plugin_data, DAY_IN_SECONDS); 
+
+        //If not
+        } else {
+            echo '<p>Something went wrong, could not get the downloaded data.</p>';
+        }
     }
 }
+
+
